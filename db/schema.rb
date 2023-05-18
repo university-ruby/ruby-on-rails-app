0953b067889b0724f33debafd295564d6e81d423
Mykyta Kasianenko Mihailovich <kasianenkomykyta@gmail.com>
2023-05-18 13:06:31 +0300

    Run migration

---
+++ db/schema.rb
@@ -0,0 +1,70 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_100501) do
+  create_table "clinics", force: :cascade do |t|
+    t.string "title"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "title"
+    t.integer "clinic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clinic_id"], name: "index_departments_on_clinic_id"
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.date "birthday_date"
+    t.integer "department_id", null: false
+    t.integer "specialization_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_doctors_on_department_id"
+    t.index ["specialization_id"], name: "index_doctors_on_specialization_id"
+  end
+
+  create_table "patient_cards", force: :cascade do |t|
+    t.string "doctor_comment"
+    t.integer "clinic_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clinic_id"], name: "index_patient_cards_on_clinic_id"
+    t.index ["patient_id"], name: "index_patient_cards_on_patient_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.date "birthday_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "specializations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "departments", "clinics"
+  add_foreign_key "doctors", "departments"
+  add_foreign_key "doctors", "specializations"
+  add_foreign_key "patient_cards", "clinics"
+  add_foreign_key "patient_cards", "patients"
+end
