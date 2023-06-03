1a157e0b869d9e6c09deec14d8a22cc19c47a652
Mykyta Kasianenko Mihailovich <kasianenkomykyta@gmail.com>
2023-06-03 16:59:26 +0300

    Added seeds and menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'menu/index'
   devise_for :users
   resources :patient_cards
   resources :patients
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,80 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require 'date'
+
+connection = ActiveRecord::Base.connection
+
+Clinic.delete_all
+Department.delete_all
+Doctor.delete_all
+Patient.delete_all
+PatientCard.delete_all
+Specialization.delete_all
+User.delete_all
+
+100.times do
+  # Clinic.create(
+  #   title: Faker::Company.name,
+  #   address: Faker::Address.street_address,
+  #   created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+  #   updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  # )
+  connection.execute(
+    "INSERT INTO clinics (title, address, created_at, updated_at) VALUES (\"#{Faker::Company.name}\", \"#{Faker::Address.street_address}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\")"
+  )
+end
+
+100.times do
+  Department.create(
+    title: Faker::Company.department,
+    clinic_id: Faker::Number.between(from: 0, to: Clinic.all.size),
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+100.times do
+  # Specialization.create(
+  #   title: Faker::Job.title,
+  #   created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+  #   updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  # )
+  connection.execute(
+    "INSERT INTO specializations (title, created_at, updated_at) VALUES (\"#{Faker::Company.name}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\")"
+  )
+end
+
+100.times do
+  Doctor.create(
+    name: Faker::Name.first_name,
+    surname: Faker::Name.last_name,
+    birthday_date: Faker::Time.between(from: DateTime.now - 10, to: DateTime.now - 5),
+    department_id: Faker::Number.between(from: 0, to: Department.all.size),
+    specialization_id: Faker::Number.between(from: 0, to: Specialization.all.size),
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
+
+
+100.times do
+  # Patient.create(
+  #   name: Faker::Name.first_name,
+  #   surname: Faker::Name.last_name,
+  #   birthday_date: Faker::Time.between(from: DateTime.now - 10, to: DateTime.now - 5),
+  #   created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+  #   updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  # )
+  connection.execute(
+    "INSERT INTO patients (name, surname, birthday_date, created_at, updated_at) VALUES (\"#{Faker::Name.first_name}\", \"#{Faker::Name.last_name}\", \"#{Faker::Time.between(from: DateTime.now - 10, to: DateTime.now - 5)}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\", \"#{Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}\")"
+  )
+end
+
+100.times do
+  PatientCard.create(
+    doctor_comment: Faker::JapaneseMedia::OnePiece.quote,
+    clinic_id: Faker::Number.between(from: 0, to: Clinic.all.size),
+    patient_id: Faker::Number.between(from: 0, to: Patient.all.size),
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  )
+end
