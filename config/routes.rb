0d874786a472368667d333fe10bd180d26d4f6c9
Mykyta Kasianenko Mihailovich <kasianenkomykyta@gmail.com>
2023-06-03 10:47:26 +0300

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :patient_cards
   resources :patients
   resources :specializations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_100501) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_074420) do
   create_table "clinics", force: :cascade do |t|
     t.string "title"
     t.string "address"
@@ -60,6 +60,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_100501) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "departments", "clinics"
